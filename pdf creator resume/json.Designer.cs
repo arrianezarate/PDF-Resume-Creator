99cefa26e691a4bebfb10e0d00742b3c7b424b26
arrianezarate <arrianenic.z@gmail.com>
2022-07-22 11:48:33 +0800

    installed pdfsharp package

--- pdf creator resume/json.Designer.cs
+++ pdf creator resume/json.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace pdf_creator_resume
 {
-    partial class json
+    partial class _frmJSON
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(_frmJSON));
+            this._tboxJSON = new System.Windows.Forms.TextBox();
+            this._btnDisplay = new System.Windows.Forms.Button();
+            this._lblJSON = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // _tboxJSON
+            // 
+            this._tboxJSON.Location = new System.Drawing.Point(77, 68);
+            this._tboxJSON.Multiline = true;
+            this._tboxJSON.Name = "_tboxJSON";
+            this._tboxJSON.Size = new System.Drawing.Size(279, 445);
+            this._tboxJSON.TabIndex = 0;
+            // 
+            // _btnDisplay
+            // 
+            this._btnDisplay.BackColor = System.Drawing.Color.HotPink;
+            this._btnDisplay.Font = new System.Drawing.Font("HP Simplified Hans", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._btnDisplay.ForeColor = System.Drawing.Color.LavenderBlush;
+            this._btnDisplay.Location = new System.Drawing.Point(135, 519);
+            this._btnDisplay.Name = "_btnDisplay";
+            this._btnDisplay.Size = new System.Drawing.Size(162, 56);
+            this._btnDisplay.TabIndex = 2;
+            this._btnDisplay.Text = "VIEW";
+            this._btnDisplay.UseVisualStyleBackColor = false;
+            this._btnDisplay.Click += new System.EventHandler(this._btnDisplay_Click);
+            // 
+            // _lblJSON
+            // 
+            this._lblJSON.AutoSize = true;
+            this._lblJSON.BackColor = System.Drawing.Color.Transparent;
+            this._lblJSON.Font = new System.Drawing.Font("HP Simplified Hans", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._lblJSON.ForeColor = System.Drawing.Color.MistyRose;
+            this._lblJSON.Location = new System.Drawing.Point(170, 29);
+            this._lblJSON.Name = "_lblJSON";
+            this._lblJSON.Size = new System.Drawing.Size(85, 36);
+            this._lblJSON.TabIndex = 3;
+            this._lblJSON.Text = "JSON";
+            // 
+            // _frmJSON
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "json";
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(436, 587);
+            this.Controls.Add(this._lblJSON);
+            this.Controls.Add(this._btnDisplay);
+            this.Controls.Add(this._tboxJSON);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "_frmJSON";
+            this.Text = "JSON ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox _tboxJSON;
+        private System.Windows.Forms.Button _btnDisplay;
+        private System.Windows.Forms.Label _lblJSON;
     }
 }
--- pdf creator resume/json.cs
+++ pdf creator resume/json.cs
@@ -10,11 +10,44 @@ using System.Windows.Forms;
 
 namespace pdf_creator_resume
 {
-    public partial class json : Form
+    public partial class _frmJSON : Form
     {
-        public json()
+        public _frmJSON()
         {
             InitializeComponent();
+        }
+
+        private void _btnDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public class ResumeContent
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Place { get; set; }
+            public string Github { get; set; }
+            public string Facebook { get; set; }
+            public string Message { get; set; }
+            public string SoftSkill_1 { get; set; }
+            public string SoftSkill_2 { get; set; }
+            public string SoftSkill_3 { get; set; }
+            public string SoftSkill_4 { get; set; }
+            public string SoftSkill_5 { get; set; }
+            public string HardSkill_1 { get; set; }
+            public string HardSkill_2 { get; set; }
+            public string HardSkill_3 { get; set; }
+            public string HardSkill_4 { get; set; }
+            public string HardSkill_5 { get; set; }
+            public string WorkExperience_1 { get; set; }
+            public string WorkExperience_2 { get; set; }
+            public string WorkExperience_3 { get; set; }
+            public string WorkExperience_4 { get; set; }
+            public string WorkExperience_5 { get; set; }
+            public string ContactNumber { get; set; }
+            public string Email { get; set; }
+            public string Address { get; set; }
         }
     }
 }
