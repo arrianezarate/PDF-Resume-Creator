7e7e466726cb519b423f9055a9e04260643fcafb
arrianezarate <arrianenic.z@gmail.com>
2022-07-22 13:05:17 +0800

    can write and read json file

--- pdf creator resume/json.Designer.cs
+++ pdf creator resume/json.Designer.cs
@@ -30,8 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(_frmJSON));
             this._tboxJSON = new System.Windows.Forms.TextBox();
-            this._btnDisplay = new System.Windows.Forms.Button();
+            this._btnWrite = new System.Windows.Forms.Button();
             this._lblJSON = new System.Windows.Forms.Label();
+            this._btnView = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // _tboxJSON
@@ -42,18 +43,18 @@
             this._tboxJSON.Size = new System.Drawing.Size(279, 445);
             this._tboxJSON.TabIndex = 0;
             // 
-            // _btnDisplay
+            // _btnWrite
             // 
-            this._btnDisplay.BackColor = System.Drawing.Color.HotPink;
-            this._btnDisplay.Font = new System.Drawing.Font("HP Simplified Hans", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this._btnDisplay.ForeColor = System.Drawing.Color.LavenderBlush;
-            this._btnDisplay.Location = new System.Drawing.Point(135, 519);
-            this._btnDisplay.Name = "_btnDisplay";
-            this._btnDisplay.Size = new System.Drawing.Size(162, 56);
-            this._btnDisplay.TabIndex = 2;
-            this._btnDisplay.Text = "VIEW";
-            this._btnDisplay.UseVisualStyleBackColor = false;
-            this._btnDisplay.Click += new System.EventHandler(this._btnDisplay_Click);
+            this._btnWrite.BackColor = System.Drawing.Color.HotPink;
+            this._btnWrite.Font = new System.Drawing.Font("HP Simplified Hans", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._btnWrite.ForeColor = System.Drawing.Color.LavenderBlush;
+            this._btnWrite.Location = new System.Drawing.Point(77, 519);
+            this._btnWrite.Name = "_btnWrite";
+            this._btnWrite.Size = new System.Drawing.Size(107, 56);
+            this._btnWrite.TabIndex = 2;
+            this._btnWrite.Text = "WRITE";
+            this._btnWrite.UseVisualStyleBackColor = false;
+            this._btnWrite.Click += new System.EventHandler(this._btnDisplay_Click);
             // 
             // _lblJSON
             // 
@@ -67,14 +68,28 @@
             this._lblJSON.TabIndex = 3;
             this._lblJSON.Text = "JSON";
             // 
+            // _btnView
+            // 
+            this._btnView.BackColor = System.Drawing.Color.HotPink;
+            this._btnView.Font = new System.Drawing.Font("HP Simplified Hans", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._btnView.ForeColor = System.Drawing.Color.LavenderBlush;
+            this._btnView.Location = new System.Drawing.Point(267, 519);
+            this._btnView.Name = "_btnView";
+            this._btnView.Size = new System.Drawing.Size(89, 56);
+            this._btnView.TabIndex = 4;
+            this._btnView.Text = "VIEW";
+            this._btnView.UseVisualStyleBackColor = false;
+            this._btnView.Click += new System.EventHandler(this._btnView_Click);
+            // 
             // _frmJSON
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(436, 587);
+            this.Controls.Add(this._btnView);
             this.Controls.Add(this._lblJSON);
-            this.Controls.Add(this._btnDisplay);
+            this.Controls.Add(this._btnWrite);
             this.Controls.Add(this._tboxJSON);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "_frmJSON";
@@ -87,7 +102,8 @@
         #endregion
 
         private System.Windows.Forms.TextBox _tboxJSON;
-        private System.Windows.Forms.Button _btnDisplay;
+        private System.Windows.Forms.Button _btnWrite;
         private System.Windows.Forms.Label _lblJSON;
+        private System.Windows.Forms.Button _btnView;
     }
 }
--- pdf creator resume/json.cs
+++ pdf creator resume/json.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace pdf_creator_resume
 {
     public partial class _frmJSON : Form
     {
+        private readonly string json_filepath = @"C:\Users\arriane\source\repos\pdf creator resume\json file\jsonInfo.JSON";
+
         public _frmJSON()
         {
             InitializeComponent();
@@ -19,14 +23,27 @@ namespace pdf_creator_resume
 
         private void _btnDisplay_Click(object sender, EventArgs e)
         {
+            try
+            {
+                var resume = GetResume();
 
+                var jsonToWrite = JsonConvert.SerializeObject(resume, Formatting.Indented);
+
+                using (var write = new StreamWriter(json_filepath))
+                {
+                    write.Write(jsonToWrite);
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
         }
 
         public class ResumeContent
         {
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public string Place { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
             public string Github { get; set; }
             public string Facebook { get; set; }
             public string Message { get; set; }
@@ -34,20 +51,67 @@ namespace pdf_creator_resume
             public string SoftSkill_2 { get; set; }
             public string SoftSkill_3 { get; set; }
             public string SoftSkill_4 { get; set; }
-            public string SoftSkill_5 { get; set; }
             public string HardSkill_1 { get; set; }
             public string HardSkill_2 { get; set; }
             public string HardSkill_3 { get; set; }
             public string HardSkill_4 { get; set; }
-            public string HardSkill_5 { get; set; }
             public string WorkExperience_1 { get; set; }
             public string WorkExperience_2 { get; set; }
             public string WorkExperience_3 { get; set; }
             public string WorkExperience_4 { get; set; }
-            public string WorkExperience_5 { get; set; }
             public string ContactNumber { get; set; }
             public string Email { get; set; }
-            public string Address { get; set; }
+        }
+
+        private void _btnView_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string jsonFile;
+                using (var read = new StreamReader(json_filepath))
+                {
+                    jsonFile = read.ReadToEnd();
+                }
+
+                _tboxJSON.Text = jsonFile;
+
+                var resumeFromJson = JsonConvert.DeserializeObject<ResumeContent>(jsonFile);
+            }
+
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+
+        private ResumeContent GetResume()
+        {
+            var resume = new ResumeContent
+            {
+
+                Name = "Arriane Nicole Zarate",
+                Address = "Cainta 1900",
+                ContactNumber = "09776886853",
+                Email = "arrianenic.z@gmail.com",
+                Facebook = "https://www.facebook.com/arrianenicole.zarate",
+                Github = "https://github.com/arrianezarate",
+                Message = "I have seen this job posting on https://thevahub/real-estate-virtual-assistant/ for an internship Real Estate Virtual Assistant. I want to become part of your company.",
+                SoftSkill_1 = "Critical Thinking Skills",
+                SoftSkill_2 = "Great attention to Data",
+                SoftSkill_3 = "Multi-tasking Skills",
+                SoftSkill_4 = "Adaptability",
+                HardSkill_1 = "Computer Literacy",
+                HardSkill_2 = "Microsoft Tools (Word, Excel, etc.)",
+                HardSkill_3 = "Web designing",
+                HardSkill_4 = "Programming and HTML Languages (C#)",
+                WorkExperience_1 = "Drive customer satisfaction through voice communications",
+                WorkExperience_2 = "Navigate through miltiple systems with a computer specifically technical skill",
+                WorkExperience_3 = "Remained courteous and calm.",
+                WorkExperience_4 = "Listen attentively to caller needs.",
+
+            };
+
+            return resume;
         }
     }
 }
